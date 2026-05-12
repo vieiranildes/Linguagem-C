cc0cec484cde33a0fdeca010592181189a2321e0
Nivalnildes Vieira <nildesvieira20@gmail.com>
2026-05-12 23:51:45 +0000

    Linguagem c

---
+++ Calculadora.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    float a, b, resultado;
+    int opcao;
+    
+    printf("\nprimeiro número"); 
+    scanf("%f", &a);
+    
+     printf("\nsegundo número"); 
+    scanf("%f", &b);
+    
+    
+    printf("---Menu");
+    printf(" \n 1. Soma");
+    printf(" \n 2. Subtração");
+    printf(" \n 3. Multiplicação");
+    printf(" \n 4. Divisão");
+    
+    scanf("%d", &opcao);
+
+if(opcao == 1){
+        resultado = a + b;
+    }else if( opcao == 2){
+        resultado = a - b;
+    }else if(opcao == 3){
+        resultado = a * b;
+    }else{ resultado = a / b;
+}
+printf("Resultado Final:  %f ", resultado);
+
+    
+
+return 0;
+}
---
+++ atividade.c
@@ -0,0 +1,42 @@
+/*
+Crie um programa que leia o nome, idade e cidade do usuário e exiba uma mensagem de apresentação formatada.
+
+Leia o nome completo com espaço
+Leia a idade como inteiro
+Leia a cidade como string
+Exiba: "Olá! Meu nome é [nome], tenho [idade] anos e sou de [cidade]."
+💡 Dicas
+
+Use char nome[50] para strings
+
+Para ler strings com espaço: scanf(" %[^\n]", nome)
+
+Use %s para exibir strings no printf
+
+
+
+*/ 
+
+#include <stdio.h>
+
+int main(){
+
+char nome[50];
+int idade;
+char cidade[50];
+
+printf("Qual seu nome: ");
+scanf("%s", &nome);
+
+printf("\nQual sua idade: ");
+scanf("%d", &idade);
+
+printf("\nQual cidade você mora: ");
+scanf("%s", &cidade);
+
+printf("Olá meu nome é %s tenho %d sou de %s", nome, idade, cidade);
+
+
+    return 0; 
+    
+}                                                                               
