81b445730a8773df0522ed214e91e2c7ebfdc36f
Nivalnildes Vieira <nildesvieira20@gmail.com>
2026-06-02 00:25:32 +0000

    Pratica

---
+++ Struct/pilha.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#define Max 10
+
+
+typedef struct{
+
+    int dados [Max];   /* os elementis */
+    int topo;  /* índice do topo*/
+
+} pilha;    /* -1 = vazia*/
+
+void inicializar(pilha *p){
+    p->topo = -1;
+}
+int estaVazia(pilha *p){
+    return p->topo == -1;
+}
+int estaCheia(pilha *p){
+    return p->topo == Max -1;
+
+}
+void push(pilha *p, int valor){
+    if(estaCheia(p)) return;
+    p->dados[++p->topo] = valor;
+}
+int pop(pilha * p){
+    if(estaVazia(p)) return -1;
+    return p->dados[p->topo--];
+}
+int peek(pilha *p){
+  if(estaVazia(p)) return -1;
+  return p->dados[p->topo];  
+}
+
+int main(){
+
+    /*uso:*/
+
+    pilha p;
+    inicializar(&p);
+    push(&p, 10);
+    push(&p, 20);
+    push(&p, 45);
+    printf("%d\n", pop(&p)); /* 20*/
+    printf("%d\n", peek(&p));
+     //pop(&p);
+    printf("%d\n", pop(&p));
+
+    return 0;
+}
